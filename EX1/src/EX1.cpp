588cd686de0eca59f0d7ea6368e16d4b1cde6a04
Or Keren <dude2110@gmail.com>
2015-03-03 21:15:58 +0200

    test

---
+++ EX1/src/EX1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : EX1.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "WHAT" << endl; // prints
+	return 0;
+}
